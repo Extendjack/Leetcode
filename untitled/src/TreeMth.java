eda3fbbb12aa00215805ad9192081184770fdacb
zhouhuilin <1547799219@qq.com>
2024-12-06 00:22:57 +0800

    二叉树的遍历方补充

--- untitled/src/TreeMth.java
+++ untitled/src/TreeMth.java
@@ -27,8 +27,9 @@ public class TreeMth {
 
     }
     //后序遍历
-
-
+    public boolean isValidBST4(TreeNode root) {
+       return dfs(root)[1]!=Long.MAX_VALUE;
+    }
     private long[] dfs(TreeNode node) {
       if (node==null){
           return new long[]{Long.MAX_VALUE,Long.MIN_VALUE};
@@ -41,17 +42,6 @@ public class TreeMth {
           return new long[]{Long.MIN_VALUE,Long.MAX_VALUE};
       }
       return new long[]{Math.min(left_v[0],x),Math.max(right_v[1],x)};
-
-
-
-
-
-
-
-
-
-
-
     }
 
 
