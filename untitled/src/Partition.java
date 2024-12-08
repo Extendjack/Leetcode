e7c617504460c6bfe3e2fc0338039c0a46a8c4c8
zhouhuilin <1547799219@qq.com>
2024-12-09 00:43:54 +0800

    回溯方法

---
+++ untitled/src/Partition.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Partition {
+    private final List<List<String>> ans = new ArrayList<>();
+    private final List<String> path = new ArrayList<>();
+    private String s;
+    public List<List<String>> partition(String s) {
+        if(s.length()==0){
+            return List.of();
+        }
+        this.s=s;
+        dfs(0, 0);
+        return ans;
+    }
+
+    private void dfs(int start, int i) {
+        if (i==s.length()){
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        if (i<s.length()-1){
+            dfs(start,i+1);
+        }
+        if(isPalindrome(start,i)){
+            path.add(s.substring(start,i+1));
+           // dfs(start,i+1);
+            dfs(i+1,i+1);
+            path.remove(path.size()-1);
+
+        }
+    }
+
+    private boolean isPalindrome(int left, int right) {
+        char[] c=s.toCharArray();
+        while (left<right){
+            if (c[left++]!=c[right--]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
---
+++ untitled/src/Subsets.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+
+    private final List<List<Integer>> ans = new ArrayList<>();
+    private final List<Integer> path = new ArrayList<>();
+    private int[] nums;
+
+    //选或者是不选
+
+    public List<List<Integer>> subsets(int[] nums) {
+        this.nums=nums;
+        int n=nums.length;
+        if(n==0){
+            return List.of();
+        }
+        dfs(0);
+        return ans;
+    }
+
+    private void dfs(int i) {
+        if (i==nums.length){
+            ans.add(new ArrayList<>(path));
+        return;
+        }
+        dfs(i+1);
+
+        path.add(nums[i]);
+        dfs(i+1);
+        path.remove(path.size()-1);
+    }
+
+
+}
