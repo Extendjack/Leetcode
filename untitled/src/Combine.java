40cc92124b2a98762256f0140246a869e2354954
zhouhuilin <1547799219@qq.com>
2024-12-10 00:19:39 +0800

    回溯方法(剪枝)

---
+++ untitled/src/Combine.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combine {
+    private int k;
+    private final List<List<Integer>> ans = new ArrayList<>();
+    private final List<Integer> path = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+          this.k=k;
+          dfs(n);
+          return ans;
+    }
+
+    private void dfs(int n) {
+        int d=k-path.size();
+        if(d==0){
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        for (int j=n;j>=d;j--){
+            path.add(j);
+            dfs(j-1);
+            path.remove(path.size()-1);
+        }
+    }
+
+
+}
