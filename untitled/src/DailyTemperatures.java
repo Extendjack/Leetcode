f482f83bf40a577170b4c9db9dcd0631c64defae
zhouhuilin <zhouhuilin@aeke.com>
2024-12-24 12:15:09 +0800

    每日温度(补充方法)

--- untitled/src/DailyTemperatures.java
+++ untitled/src/DailyTemperatures.java
@@ -6,11 +6,15 @@ import java.util.ArrayDeque;
  *
  **/
 public class DailyTemperatures {
+    /**
+     *从右到左
+     *
+     **/
     public int[] dailyTemperatures(int[] temperatures) {
 
         int n = temperatures.length;
         int[] ans = new int[n];
-        Deque<Integer> st = new ArrayDeque<>();
+        ArrayDeque<Integer> st = new ArrayDeque<>();
 
         for (int i=n-1;i>=0;i--){
             int v=temperatures[i];
@@ -23,9 +27,28 @@ public class DailyTemperatures {
             st.push(i);
         }
         return ans;
+    }
+    /**
+     * 从左到右
+     **/
 
+    public int[] dailyTemperatures2(int[] temperatures) {
+     int n=temperatures.length;
+     int[] ans=new int[n];
+     ArrayDeque<Integer> st=new ArrayDeque<>();
+     for(int i=0;i<n;i++){
+         int v=temperatures[i];
+         while (!st.isEmpty()&&v>=temperatures[st.peek()]){
+             int j=st.pop();
+             ans[i]=i-j;
+         }
+         st.push(i);
+     }
+     return ans;
 
     }
+
+
 
 
 }
